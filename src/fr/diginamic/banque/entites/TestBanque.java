06f1e6a1bcabfe314e5954b395c20b38e80d7352
Maxence Bon <maxence.bon@orange.fr>
2022-11-23 17:42:01 +0100

    tp

--- src/fr/diginamic/banque/entites/TestBanque.java
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -6,12 +6,14 @@ public class TestBanque {
 		// TODO Auto-generated method stub
 		Compte compteNormal1 = new Compte("16B56S445", 200.45f);
 		CompteTaux compteTaux1 = new CompteTaux("16B56S445", 200.45f, 3);
+		Compte compteNormal2 = new Compte("16sd5", 154f);
 
 		System.out.println(compteTaux1.toString());
 		
-		String[] tableauCompte = new String[2];
+		String[] tableauCompte = new String[3];
 		tableauCompte[0]=compteNormal1.toString();
 		tableauCompte[1]=compteTaux1.toString();
+		tableauCompte[2]=compteNormal2.toString();
 		
 		String[] tableauCompteTest= {compteNormal1.toString(),compteTaux1.toString()};
 		
